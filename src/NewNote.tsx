3192592ac1412e2b6569f4fc5fdd942dc7d97d06
Denis Gichana <denisgichana162@gmail.com>
2023-04-02 18:00:00 +0200

    initial commit

--- src/NewNote.tsx
+++ src/NewNote.tsx
@@ -1,17 +1,21 @@
-import React from 'react'
-import { NoteData } from './App'
-import NoteForm from './NoteForm'
+import { NoteData, Tag } from "./App"
+import { NoteForm } from "./NoteForm"
 
 type NewNoteProps = {
     onSubmit: (data: NoteData) => void
+    onAddTag: (tag: Tag) => void
+    availableTags: Tag[]
 }
-function NewNote({ onSubmit }: NewNoteProps) {
+
+export function NewNote({ onSubmit, onAddTag, availableTags }: NewNoteProps) {
     return (
         <>
             <h1 className="mb-4">New Note</h1>
-            <NoteForm onSubmit={onSubmit} />
+            <NoteForm
+                onSubmit={onSubmit}
+                onAddTag={onAddTag}
+                availableTags={availableTags}
+            />
         </>
     )
 }
-
-export default NewNote
